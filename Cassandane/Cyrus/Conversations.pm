16cba8b75ea69235d4c8bc3202ae9c09d75e34d3
Greg Banks <gnb@fastmail.fm>
2011-10-13 15:04:47 +1100

    Add tests for XCONVSORT (v2).

--- Cassandane/Cyrus/Conversations.pm
+++ Cassandane/Cyrus/Conversations.pm
@@ -47,6 +47,7 @@ use DateTime;
 use URI::Escape;
 use Digest::SHA1 qw(sha1_hex);
 use Cassandane::ThreadedGenerator;
+use Cassandane::Message;
 use Cassandane::Util::Log;
 use Cassandane::Util::DateTime qw(to_iso8601 from_iso8601
 				  from_rfc822
@@ -407,6 +408,536 @@ sub test_fm_webui_draft
     $self->{store}->write_message($exp{B});
     $self->{store}->write_end();
     $self->check_messages(\%exp);
+}
+
+#
+# XCONVSORT and XCONVUPDATES tests
+#
+
+sub make_threaded_messages
+{
+    my ($self, %params) = @_;
+    my $store = $self->{store};
+
+    xlog "generating some messages";
+    my $gen = Cassandane::ThreadedGenerator->new(%params);
+    my %msgs;
+    my %cids;
+    my $uid = 1;
+
+    $store->write_begin();
+    while (my $msg = $gen->generate(extra_lines => int(rand(50))))
+    {
+	$store->write_message($msg);
+
+	# work out the cid
+	my $threadid = $msg->get_header('X-Cassandane-Thread');
+	my $cid = $cids{$threadid};
+	$cid = $cids{$threadid} = calc_cid($msg)
+	    unless defined $cid;
+
+	$msg->set_attributes(uid => $uid, cid => $cid);
+	$msgs{$uid} = $msg;
+	$uid++;
+    }
+    $store->write_end();
+
+    return (\%msgs, scalar(keys %msgs), scalar(keys %cids));
+}
+
+sub expected_uids
+{
+    my ($msgs, %params) = @_;
+    my $conversations = $params{conversations} || 0;
+    my $search = $params{search} || sub { return $_; };
+    my $sort = $params{sort} || sub { $a->uid <=> $b->uid; };
+    my $position = $params{position} || 1;
+    my $limit = $params{limit};
+    my $anchor = $params{anchor};
+    my $offset = $params{offset} || 0;
+    my $anchor_pos;
+    my %seen;
+    my $pos = 1;
+    my @res;
+
+    my @subset = values %$msgs;
+    @subset = grep $search, @subset;
+    @subset = sort $sort @subset;
+
+    foreach my $msg (@subset)
+    {
+	my $uid = $msg->get_attribute('uid');
+
+	if ($conversations)
+	{
+	    my $cid = $msg->get_attribute('cid');
+	    next if defined $seen{$cid};
+	    $seen{$cid} = 1;
+	}
+
+	$anchor_pos = $pos
+	    if (defined $anchor && $anchor == $uid);
+
+	push(@res, $uid);
+	$pos++;
+    }
+
+    if (defined $anchor)
+    {
+	$limit = scalar(@res) unless defined $limit;
+	$anchor_pos = scalar(@res)+1 unless defined $anchor_pos;
+	$anchor_pos += $offset;
+	@res = splice(@res,$anchor_pos-1,$limit);
+    }
+    elsif (defined $position)
+    {
+	$limit = scalar(@res) unless defined $limit;
+	@res = splice(@res,$position-1,$limit);
+    }
+
+    return \@res;
+}
+
+sub check_convsort
+{
+    my ($self, $messages, $res, %params) = @_;
+    my $exp = expected_uids($messages, %params);
+    xlog "expecting uids: " . join(" ",@$exp);
+    $self->assert_deep_equals($exp, $res);
+}
+
+sub convsort
+{
+    my ($talk, @args) = @_;
+    return $talk->_imap_cmd('xconvsort', 0, 'sort', @args);
+}
+
+sub convsort_postcondition
+{
+    my ($self, $exp_uidn, $exp_hms, $exp_pos, $exp_total) = @_;
+
+    my $talk = $self->{store}->get_client();
+
+    $self->assert_str_equals('ok', $talk->get_last_completion_response());
+
+    $self->assert_num_equals($exp_uidn,
+			     $talk->get_response_code('uidnext'));
+    $self->assert_num_equals($exp_hms,
+			     $talk->get_response_code('highestmodseq'));
+    if (defined $exp_pos)
+    {
+	$self->assert_num_equals($exp_pos,
+				 $talk->get_response_code('position'));
+    }
+    else
+    {
+	$self->assert_null($talk->get_response_code('position'));
+    }
+    $self->assert_num_equals($exp_total,
+			     $talk->get_response_code('total'));
+
+    $talk->clear_response_code('uidnext');
+    $talk->clear_response_code('highestmodseq');
+    $talk->clear_response_code('position');
+    $talk->clear_response_code('total');
+}
+
+sub test_convsort_window
+{
+    my ($self) = @_;
+
+    # check IMAP server has the XCONVERSATIONS capability
+    $self->assert($self->{store}->get_client()->capability()->{xconversations});
+
+    my $folder = 'INBOX';
+    my $store = $self->{store};
+    my $talk = $store->get_client();
+
+    my ($messages, $nmessages, $nthreads) =
+	    $self->make_threaded_messages(nmessages => 100, nthreads => 15);
+    my $exp_uidn = $nmessages+1;
+    my $exp_hms = $nmessages+4,
+    my $res;
+    my $exp;
+    my $anchor;
+
+    xlog "xconvsort in message mode with no window-args";
+    $res = convsort($talk, $folder, ['uid'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids in uid order";
+    $self->check_convsort($messages, $res);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nmessages);
+
+    xlog "xconvsort in conversation mode with no other window-args";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect the conversation exemplars in uid order";
+    $self->check_convsort($messages, $res, conversations => 1);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nthreads);
+
+    xlog "xconvsort in message mode with POSITION and no LIMIT";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['position', [5, 0]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids from 5 onward in uid order";
+    $self->check_convsort($messages, $res, position => 5);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nmessages);
+
+    xlog "xconvsort in conversation mode with POSITION and no LIMIT";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'position', [5, 0]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the exemplars from the 5th onward in uid order";
+    $self->check_convsort($messages, $res,
+			  conversations => 1, position => 5);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nthreads);
+
+    xlog "xconvsort in message mode with POSITION and LIMIT";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['position', [5, 7]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect 7 uids from 5 onward in uid order";
+    $self->check_convsort($messages, $res,
+			  position => 5, limit => 7);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nmessages);
+
+    xlog "xconvsort in conversation mode with POSITION and LIMIT";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'position', [5, 7]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect 7 exemplars from the 5th onward in uid order";
+    $self->check_convsort($messages, $res,
+			  conversations => 1, position => 5, limit => 7);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nthreads);
+
+    xlog "xconvsort in message mode with POSITION and LIMIT=1";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['position', [5, 1]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect 1 uids from 5 onward in uid order";
+    $self->check_convsort($messages, $res,
+			  position => 5, limit => 1);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nmessages);
+
+    xlog "xconvsort in conversation mode with POSITION and LIMIT=1";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'position', [5, 1]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect 1 exemplars from the 5th onward in uid order";
+    $self->check_convsort($messages, $res,
+			  conversations => 1, position => 5, limit => 1);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nthreads);
+
+    xlog "xconvsort in message mode with POSITION off the end";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['position', [$nmessages+1, 7]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect no uids";
+    $self->assert_deep_equals([], $res);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, undef, $nmessages);
+
+    xlog "xconvsort in conversation mode with POSITION off the end";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'position', [$nthreads+1, 7]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect no exemplars";
+    $self->assert_deep_equals([], $res);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, undef, $nthreads);
+
+    xlog "xconvsort in message mode with ANCHOR not found";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['anchor', [$nmessages+1, 0, 0]],
+		    'us-ascii', 'all');
+    $self->assert_str_equals('no', $talk->get_last_completion_response());
+    $self->assert($talk->get_last_error() =~ m/anchor not found/i);
+
+    xlog "xconvsort in conversation mode with ANCHOR not found";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'anchor', [$nmessages+1, 0, 0]],
+		    'us-ascii', 'all');
+    $self->assert_str_equals('no', $talk->get_last_completion_response());
+    $self->assert($talk->get_last_error() =~ m/anchor not found/i);
+
+    xlog "xconvsort in message mode with ANCHOR and POSITION";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['anchor', [1, 0, 0], 'position', [1, 0]],
+		    'us-ascii', 'all');
+    $self->assert_str_equals('bad', $talk->get_last_completion_response());
+    $self->assert($talk->get_last_error() =~ m/syntax error in window/i);
+
+    xlog "xconvsort in conversation mode with ANCHOR and POSITION";
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'anchor', [1, 0, 0], 'position', [1, 0]],
+		    'us-ascii', 'all');
+    $self->assert_str_equals('bad', $talk->get_last_completion_response());
+    $self->assert($talk->get_last_error() =~ m/syntax error in window/i);
+
+    xlog "xconvsort in message mode with ANCHOR and no OFFSET or LIMIT";
+    $anchor = expected_uids($messages, conversations => 0,
+			    position => 5, limit => 1)->[0];
+    $res = convsort($talk, $folder, ['uid'],
+		    ['anchor', [$anchor, 0, 0]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids from 5 onward in uid order";
+    $self->check_convsort($messages, $res, anchor => $anchor);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nmessages);
+
+    xlog "xconvsort in conversation mode with ANCHOR and no OFFSET or LIMIT";
+    $anchor = expected_uids($messages, conversations => 1,
+			    position => 5, limit => 1)->[0];
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'anchor', [$anchor, 0, 0]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the exemplars from the 5th conversation in uid order";
+    $self->check_convsort($messages, $res,
+			  conversations => 1, anchor => $anchor);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 5, $nthreads);
+
+    xlog "xconvsort in message mode with ANCHOR and OFFSET but no LIMIT";
+    $anchor = expected_uids($messages, conversations => 0,
+			    position => 5, limit => 1)->[0];
+    $res = convsort($talk, $folder, ['uid'],
+		    ['anchor', [$anchor, 3, 0]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids from 8 onward in uid order";
+    $self->check_convsort($messages, $res,
+			  anchor => $anchor, offset => 3);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 8, $nmessages);
+
+    xlog "xconvsort in conversation mode with ANCHOR and OFFSET but no LIMIT";
+    $anchor = expected_uids($messages, conversations => 1,
+			    position => 5, limit => 1)->[0];
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'anchor', [$anchor, 3, 0]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the exemplars from the 8th conversation in uid order";
+    $self->check_convsort($messages, $res,
+			  conversations => 1, anchor => $anchor,
+			  offset => 3);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 8, $nthreads);
+
+    xlog "xconvsort in message mode with ANCHOR and OFFSET and LIMIT";
+    $anchor = expected_uids($messages, conversations => 0,
+			    position => 5, limit => 1)->[0];
+    $res = convsort($talk, $folder, ['uid'],
+		    ['anchor', [$anchor, 3, 7]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect 7 uids from 8 onward in uid order";
+    $self->check_convsort($messages, $res,
+			  anchor => $anchor, offset => 3, limit => 7);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 8, $nmessages);
+
+    xlog "xconvsort in conversation mode with ANCHOR and OFFSET and LIMIT";
+    $anchor = expected_uids($messages, conversations => 1,
+			    position => 5, limit => 1)->[0];
+    $res = convsort($talk, $folder, ['uid'],
+		    ['conversations', 'anchor', [$anchor, 3, 7]],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect 7 exemplars from the 8th conversation in uid order";
+    $self->check_convsort($messages, $res,
+			  conversations => 1, anchor => $anchor,
+			  offset => 3, limit => 7);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 8, $nthreads);
+}
+
+sub from_cmp
+{
+    my ($afrom) = ($a->from =~ m/<([^@>]*)/);
+    my ($bfrom) = ($b->from =~ m/<([^@>]*)/);
+
+    return lc($afrom) cmp lc($bfrom) ||
+	   $a->uid <=> $b->uid;
+}
+
+sub reverse_from_cmp
+{
+    my ($afrom) = ($a->from =~ m/<([^@>]*)/);
+    my ($bfrom) = ($b->from =~ m/<([^@>]*)/);
+
+    # Note: FROM is REVERSEd but the default UID sort isn't
+    return lc($bfrom) cmp lc($afrom) ||
+	   $a->uid <=> $b->uid;
+}
+
+sub to_cmp
+{
+    my ($ato) = ($a->to =~ m/<([^@>]*)/);
+    my ($bto) = ($b->to =~ m/<([^@>]*)/);
+
+    return lc($ato) cmp lc($bto) ||
+	   $a->uid <=> $b->uid;
+}
+
+sub subject_cmp
+{
+    return base_subject($a->subject) cmp base_subject($b->subject) ||
+	   $a->uid <=> $b->uid;
+}
+
+sub size_cmp
+{
+    return $a->size <=> $b->size ||
+	   $a->uid <=> $b->uid;
+}
+
+sub from_reverse_size_subject_cmp
+{
+    my ($afrom) = ($a->from =~ m/<([^@>]*)/);
+    my ($bfrom) = ($b->from =~ m/<([^@>]*)/);
+
+    return lc($afrom) cmp lc($bfrom) ||
+	   $b->size <=> $a->size ||
+	   base_subject($a->subject) cmp base_subject($b->subject) ||
+	   $a->uid <=> $b->uid;
+}
+
+sub test_convsort_sort
+{
+    my ($self) = @_;
+
+    # check IMAP server has the XCONVERSATIONS capability
+    $self->assert($self->{store}->get_client()->capability()->{xconversations});
+
+    my $folder = 'INBOX';
+    my $store = $self->{store};
+    my $talk = $store->get_client();
+
+    my ($messages, $nmessages, $nthreads) =
+	    $self->make_threaded_messages(nmessages => 100, nthreads => 15);
+    my $exp_uidn = $nmessages+1;
+    my $exp_hms = $nmessages+4,
+    my $res;
+    my $exp;
+    my $anchor;
+
+    xlog "xconvsort in message mode sorting on FROM";
+    $res = convsort($talk, $folder, ['from'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids in FROM order";
+    $self->check_convsort($messages, $res, sort => \&from_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nmessages);
+
+    xlog "xconvsort in conversation mode sorting on FROM";
+    $res = convsort($talk, $folder, ['from'],
+		    ['conversations'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect the conversation exemplars in FROM order";
+    $self->check_convsort($messages, $res, conversations => 1,
+			  sort => \&from_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nthreads);
+
+    xlog "xconvsort in message mode sorting on REVERSE FROM";
+    $res = convsort($talk, $folder, ['reverse', 'from'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids in REVERSE FROM order";
+    $self->check_convsort($messages, $res, sort => \&reverse_from_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nmessages);
+
+    xlog "xconvsort in conversation mode sorting on REVERSE FROM";
+    $res = convsort($talk, $folder, ['reverse', 'from'],
+		    ['conversations'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect the conversation exemplars in REVERSE FROM order";
+    $self->check_convsort($messages, $res, conversations => 1,
+			  sort => \&reverse_from_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nthreads);
+
+    # TO order isn't very interesting, all the messages are generated
+    # with the same To: address anyway.
+    xlog "xconvsort in message mode sorting on TO";
+    $res = convsort($talk, $folder, ['to'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids in TO order";
+    $self->check_convsort($messages, $res, sort => \&to_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nmessages);
+
+    xlog "xconvsort in conversation mode sorting on TO";
+    $res = convsort($talk, $folder, ['to'],
+		    ['conversations'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect the conversation exemplars in TO order";
+    $self->check_convsort($messages, $res, conversations => 1,
+			  sort => \&to_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nthreads);
+
+    xlog "xconvsort in message mode sorting on SUBJECT";
+    $res = convsort($talk, $folder, ['subject'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids in SUBJECT order";
+    $self->check_convsort($messages, $res, sort => \&subject_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nmessages);
+
+    xlog "xconvsort in conversation mode sorting on SUBJECT";
+    $res = convsort($talk, $folder, ['subject'],
+		    ['conversations'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect the conversation exemplars in SUBJECT order";
+    $self->check_convsort($messages, $res, conversations => 1,
+			  sort => \&subject_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nthreads);
+
+    xlog "xconvsort in message mode sorting on SIZE";
+    $res = convsort($talk, $folder, ['size'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids in SIZE order";
+    $self->check_convsort($messages, $res, sort => \&size_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nmessages);
+
+    xlog "xconvsort in conversation mode sorting on SIZE";
+    $res = convsort($talk, $folder, ['size'],
+		    ['conversations'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect the conversation exemplars in SIZE order";
+    $self->check_convsort($messages, $res, conversations => 1,
+			  sort => \&size_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nthreads);
+
+    xlog "xconvsort in message mode sorting on FROM REVERSE SIZE SUBJECT";
+    $res = convsort($talk, $folder, ['from', 'reverse', 'size', 'subject'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect all the uids in FROM REVERSE SIZE SUBJECT order";
+    $self->check_convsort($messages, $res,
+			  sort => \&from_reverse_size_subject_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nmessages);
+
+    xlog "xconvsort in conversation mode sorting on FROM REVERSE SIZE
+    SUBJECT";
+    $res = convsort($talk, $folder, ['from', 'reverse', 'size', 'subject'],
+		    ['conversations'],
+		    'us-ascii', 'all')
+	or die "xconvsort failed: $@";
+    xlog "expect the conversation exemplars in FROM REVERSE SIZE SUBJECT order";
+    $self->check_convsort($messages, $res, conversations => 1,
+			  sort => \&from_reverse_size_subject_cmp);
+    $self->convsort_postcondition($exp_uidn, $exp_hms, 1, $nthreads);
 }
 
 1;
